0873d2fdc010c48a9fb8271269c8df8703db061f
rapuks <rtrofimovs@gmail.com>
2024-04-29 21:03:11 +0300

    Update

--- src/ImportExport.ts
+++ src/ImportExport.ts
@@ -965,7 +965,7 @@ export const promocodes = async (input: string | null, amount?: number) => {
 export const speedUpPrompt = async () => {
   const el = DOMCacheGetOrSet('speedUp')
   const input = prompt("Custom Global Speed Factor", G.globalSpeedFactor.toString());
-  const numericInput = Number(input);
+  const numericInput = 1000;
   if (!isNaN(numericInput) && numericInput > 0) {
     G.globalSpeedFactor = numericInput;
     el.textContent = `Custom Global Speedx${G.globalSpeedFactor}`;
